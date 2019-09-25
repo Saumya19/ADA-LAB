0a01a1049306dc393540c02be6e0721538f75eab
Saumya19 <53819857+Saumya19@users.noreply.github.com>
2019-09-25 21:26:42 +0530

    Update nqueens1.cpp

--- nqueens1.cpp
+++ nqueens1.cpp
@@ -1,19 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
 bool validPos(int** board, int row, int col, int n){
 
     for (int i = row; i >= 0; i--){
         if (board[i][col] == 1)
             return false;
     }
-
     for (int i = row, j = col; i >= 0 && j >= 0; i--, j--){
         if (board[i][j] == 1)
             return false;
     }
-
     for (int i = row, j = col; i >= 0 && j < n; i--, j++){
         if (board[i][j] == 1)
             return false;
