13638f5c325f30ab9972c7664e51a00935be0c34
Saumya19 <53819857+Saumya19@users.noreply.github.com>
2019-09-25 21:26:13 +0530

    Update nqueens1.cpp

--- nqueens1.cpp
+++ nqueens1.cpp
@@ -1,38 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int x[20],counti=0;
-bool place(int,int);
-void nqueens(int k,int n);
+
+
+bool validPos(int** board, int row, int col, int n){
+
+    for (int i = row; i >= 0; i--){
+        if (board[i][col] == 1)
+            return false;
+    }
+
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--){
+        if (board[i][j] == 1)
+            return false;
+    }
+
+    for (int i = row, j = col; i >= 0 && j < n; i--, j++){
+        if (board[i][j] == 1)
+            return false;
+    }
+
+    return true;
+}
+
+void nQueen(int** board, int n, int row){
+
+    if (row == n){
+        cout << "Queen Placement" << endl;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                cout << board[i][j] <<" ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+        return;
+    }
+
+    for (int j = 0; j < n; j++){
+        if (validPos(board, row, j, n)){
+            board[row][j] = 1;
+            nQueen(board, n, row + 1);
+            board[row][j] = 0;
+        }
+    }
+    return;
+}
+
+
 int main(){
-int n;
-cout<<"enter n:";
-cin>>n;
-nqueens(1,n);
-return 0;
-}
+    int n;
+    cout << "Enter the number of queens : ";
+    cin >> n;
+    int** board = new int* [n];
+    for (int i = 0; i < n; i++){
+        board[i] = new int[n];
+        for (int j = 0; j < n; j++)
+            board[i][j] = 0;
+    }
 
-void nqueens(int k,int n){
-for(int i=1;i<=n;i++){
-if(place(k,i)){
-x[k]=i;
-if(k==n){
-for(int j=1;j<=n;j++){
-cout<<j<<"th queen at:"<<x[j]<<endl;
+    nQueen(board, n, 0);
+    return 0;
 }
-}
-else{
-nqueens(k+1,n);
-}
-}
-}
-}
-bool place(int k,int i){
-for(int j=1;j<=k-1;j++){
-if((x[j]==i) or(abs(x[j]-i)==abs(j-k)))
-{
-return false;
-}
-}
-return true;
-}
-
